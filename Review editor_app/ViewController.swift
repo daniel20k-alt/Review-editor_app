aeccaab283eaea9eec4c0ab9f770b60b6fc16375
Testing_author <testing_beta@mail.com>
2020-10-01 23:25:06 +0300

    Initial Commit

---
+++ Review editor_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Review editor_app
+//
+//  Created by DDDD on 01/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
